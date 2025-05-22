4121bbf5246eb333ca287f98714be419c84f9c75
Tong LI <tl10@sanger.ac.uk>
2025-05-22 14:52:56 +0000

    do not perform per well step any more. generate all files in a plain folder and a multiwell plate companion file; update snapshot

--- modules/sanger/imaging/generatecompanion/resources/usr/bin/generate_companion.py
+++ modules/sanger/imaging/generatecompanion/resources/usr/bin/generate_companion.py
@@ -5,40 +5,62 @@ import fire
 from aicsimageio import AICSImage
 import numpy as np
 from ome_types import to_xml
+from ome_types.model import TiffData
 import pandas as pd
 import os
+import uuid
 
+
+def generate_tiles_to_process_csv(
+    img_obj: AICSImage,
+):
+    """
+    Generate a CSV file with the tiles to process.
+    """
+    df = pd.DataFrame(
+        {
+            "image_id" : [img_obj.metadata.images[i].id for i in np.arange(len(img_obj.scenes))]
+        }
+    )
+    df["index"] = np.arange(len(img_obj.scenes))
+    return df
+
+def append_tiffdata_to_ome_metadata(
+    img_obj: AICSImage,
+    index: int,
+):
+    """
+    Append TiffData to the OME metadata.
+    """
+    current_image = img_obj.ome_metadata.images[index]
+    filename = current_image.id + ".tif"
+    tiff_uuid = TiffData.UUID(
+        value=f"urn:uuid:{uuid.uuid4()}",
+        file_name=filename
+        )
+    tiff = TiffData(
+        first_c=0,
+        first_t=0,
+        first_z=0,
+        uuid=tiff_uuid
+    )
+    img_obj.ome_metadata.images[index].pixels.tiff_data_blocks.append(tiff)
 
 def main(file_with_ome_md, tiles_csv, companion_xml, master_file="Index.idx.xml"):
     """
     Generate a companion file for a given image file.
     """
     img = AICSImage(f"{file_with_ome_md}/{master_file}")
-    md = img.metadata
-
-    wells = []
-    if len(md.plates) > 0:
-        # Plate format
-        for well in md.plates[0].wells:
-            print(well)
-            for fov in well.well_samples:
-                row = fov.model_dump()
-                fov_md = {}
-                fov_md["index"] = fov.index
-                fov_md['well_id'] = well.id
-                fov_md['fov_id'] = row['image_ref']['id']
-                wells.append(fov_md)
-    else:
-        raise ValueError("No plates found in the metadata.")
-    df = pd.DataFrame(wells)
-    df["root_folder"] = os.path.realpath(file_with_ome_md)
-
+    df = generate_tiles_to_process_csv(img)
+    df["root_xml"] = os.path.realpath(file_with_ome_md)
     df.to_csv(tiles_csv, index=False)
 
+    for i in np.arange(len(img.scenes)):
+        append_tiffdata_to_ome_metadata(img, i)
+
     # Save the OME metadata as an XML file
-    ome_str = to_xml(md)
     with open(companion_xml, "w") as xml_file:
-        xml_file.write(ome_str)
+        xml_file.write(to_xml(img.ome_metadata))
 
 def version():
     """
